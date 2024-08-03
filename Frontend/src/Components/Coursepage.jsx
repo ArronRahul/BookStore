a7e0845ef982497624b6a14450fbef072865175a
RAHUL A P <rahul.ap@aot-technologies.com>
2024-08-02 20:16:00 -0700

    morning

--- Frontend/src/Components/Coursepage.jsx
+++ Frontend/src/Components/Coursepage.jsx
@@ -6,8 +6,9 @@ export default function Coursepage() {
   return (
     <>
     <div className='max-w-screen-2xl container mx-auto md:px-20 px-4 bg-white'>
-      <div className='mt-20 bg-white'>
+      <div className='pt-20 items-center justify-center text-center'>
         <h1 className='text-2xl font-semibold md:px-20 px-4'>We're delighted to have you <span className='text-pink-500'>Here :)</span></h1>
+        <p></p>
       </div>
     </div>
     </>
